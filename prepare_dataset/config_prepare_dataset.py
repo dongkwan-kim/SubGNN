39c6c573892acb7478b733c6b47a10b783af58df
Dongkwan Kim <todoaskit@gmail.com>
2021-12-08 21:48:25 +0900

    Add configs for synthetic datasets

--- prepare_dataset/config_prepare_dataset.py
+++ prepare_dataset/config_prepare_dataset.py
@@ -11,21 +11,69 @@ GENERATE_NODE_EMB = True  # whether to generate node embeddings
 # Random Seed
 RANDOM_SEED = 42
 
+DESIRED_PROPERTY = "density"  # density, cut_ratio, cc, coreness
+
 # Parameters for generating synthetic subgraphs with specific properties
-DESIRED_PROPERTY = "density"
-BASE_GRAPH_TYPE = "barabasi_albert"
-SUBGRAPH_TYPE = "bfs"
-N_SUBGRAPHS = 250
-N_CONNECTED_COMPONENTS = 1
-N_SUBGRAPH_NODES = 20
-FEATURES_TYPE = "one_hot"
-N = 1000
-P = 0.5
-Q = 0
-M = 5
-N_BINS = 3
-SUBGRAPH_GENERATOR = "complete"
-MODIFY_GRAPH_FOR_PROPERTIES = True
+# https://github.com/mims-harvard/SubGNN/tree/main/prepare_dataset#prepare-dataset
+if DESIRED_PROPERTY == "density":
+    BASE_GRAPH_TYPE = "barabasi_albert"
+    SUBGRAPH_TYPE = "bfs"
+    N_SUBGRAPHS = 250
+    N_CONNECTED_COMPONENTS = 1
+    N_SUBGRAPH_NODES = 20
+    FEATURES_TYPE = "one_hot"
+    N = 1000
+    P = 0.5
+    Q = 0
+    M = 5
+    N_BINS = 3
+    SUBGRAPH_GENERATOR = "complete"
+    MODIFY_GRAPH_FOR_PROPERTIES = True
+
+elif DESIRED_PROPERTY == "cut_ratio":
+    BASE_GRAPH_TYPE = 'barabasi_albert'
+    SUBGRAPH_TYPE = 'plant'
+    N_SUBGRAPHS = 250
+    N_CONNECTED_COMPONENTS = 1
+    N_SUBGRAPH_NODES = 20
+    FEATURES_TYPE = 'one_hot'
+    N = 5000
+    P = 0.5  # default
+    Q = 0  # not used
+    M = 5  # default
+    N_BINS = 3  # not used
+    SUBGRAPH_GENERATOR = 'complete'
+    MODIFY_GRAPH_FOR_PROPERTIES = True
+
+elif DESIRED_PROPERTY == "cc":
+    BASE_GRAPH_TYPE = 'barabasi_albert'
+    SUBGRAPH_TYPE = 'staple'
+    N_SUBGRAPHS = 250
+    N_CONNECTED_COMPONENTS = None
+    N_SUBGRAPH_NODES = 15
+    FEATURES_TYPE = 'one_hot'
+    N = 1000
+    P = 0.5  # default
+    Q = 0
+    M = 5  # default
+    N_BINS = 2  # not used
+    SUBGRAPH_GENERATOR = 'extended_barabasi_albert'
+    MODIFY_GRAPH_FOR_PROPERTIES = True
+
+elif DESIRED_PROPERTY == "coreness":
+    BASE_GRAPH_TYPE = 'duplication_divergence_graph'
+    SUBGRAPH_TYPE = 'plant'
+    N_SUBGRAPHS = 30  # Number of subgraphs per coreness value; N_SUBGRAPHS = 30 results in ~250 total subgraphs
+    N_CONNECTED_COMPONENTS = 1
+    N_SUBGRAPH_NODES = 20
+    FEATURES_TYPE = 'one_hot'
+    N = 5000
+    P = 0.7
+    Q = 0  # not used (Q = 1 - P)
+    M = 1
+    N_BINS = 3
+    SUBGRAPH_GENERATOR = 'duplication_divergence_graph'
+    MODIFY_GRAPH_FOR_PROPERTIES = True
 
 # Output directory ('density' as an example)
 DATASET_DIR = Path(general_config.PROJECT_ROOT) / DESIRED_PROPERTY.upper()
