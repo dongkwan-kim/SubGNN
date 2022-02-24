c609bcb4d7243dead899eac4b86166e76c0fdc77
Dongkwan Kim <todoaskit@gmail.com>
2022-02-24 19:48:22 +0900

    Update PROJECT_ROOT

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # directory where data and results will be stored
 # TODO: *UPDATE TO YOUR DIRECTORY OF CHOICE*
-PROJECT_ROOT = Path('/mnt/subgraphs/data_to_release')
+PROJECT_ROOT = Path('/root/dongkwan_tmp/SubGNN_data')
 
 # padding
 PAD_VALUE = 0
